965d3a75bea79504172f9e1bdc8389f42f87f02f
Adam Newgas <boris@boristhebrave.com>
2013-11-27 21:58:40 +0000

    First Attempt

--- celtic.py
+++ celtic.py
@@ -16,8 +16,37 @@ class CelticOperator(bpy.types.Operator):
     bl_idname = "object.celtic_operator"
     bl_label = "Celtic Operator"
 
+    total = bpy.props.IntProperty(name="Steps", default=2, min=1, max=100)
+
     def execute(self, context):
-        context.active_object.location.x += 1.0
+        obj = context.active_object
+        assert obj.type == "MESH"
+        mesh = bpy.data.meshes.new("Celtic")
+        obj = obj.data
+        verts = []
+        edges = []
+        faces = []
+        for e in obj.edges.values():
+            v1 = obj.vertices[e.vertices[0]]
+            v2 = obj.vertices[e.vertices[1]]
+            m = tuple((v1.co[i]+v2.co[i])/2 for i in range(3))
+            verts.append(m)
+        for f in obj.polygons.values():
+            #assert len(f.vertices) == 3, repr(f.vertices)
+            fout = []
+            for i in range(f.loop_start,f.loop_start+f.loop_total):
+                n = i+1
+                if n == f.loop_start+f.loop_total: n = f.loop_start
+                l1 = obj.loops[i]
+                l2 = obj.loops[n]
+                edges.append((l1.edge_index,l2.edge_index))
+                fout.append(l1.edge_index)
+            #faces.append(fout)
+
+        mesh.from_pydata(verts, edges, faces)
+        mesh.update()
+        from bpy_extras import object_utils
+        object_utils.object_data_add(context, mesh, operator=None)
         return {'FINISHED'}
 
 def register():
